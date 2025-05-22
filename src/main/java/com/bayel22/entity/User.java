119c9685d73f9b0880c4fff0d4901f7a1bd903c5
bayel22 <bayel.mamatazimov22@gmail.com>
2025-05-22 23:37:56 +0300

    update User and repository

--- src/main/java/com/bayel22/entity/User.java
+++ src/main/java/com/bayel22/entity/User.java
@@ -1,4 +1,19 @@
 package com.bayel22.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class User {
+
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    private String email;
+    private String password;
 }
--- src/main/java/com/bayel22/repository/UserRepository.java
+++ src/main/java/com/bayel22/repository/UserRepository.java
@@ -1,4 +1,25 @@
 package com.bayel22.repository;
 
+import com.bayel22.entity.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Getter
+@Setter
 public class UserRepository {
+    private List<User> users = new ArrayList<User>(Arrays.asList(
+            new User(1L, "hi1", "h2", "@gmail", "123"),
+            new User(2L, "hi2", "h2", "@gmail", "1234")
+    ));
+
+    public User getUserById(Long id) {
+        return users.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
 }
